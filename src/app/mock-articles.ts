fefe7c0d3d33641cf262c72369342406273c5e90
panvicka <panvicka@gmail.com>
2020-06-20 09:16:32 +0200

    added mock articles and css

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,34 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+    {
+        id: 1,
+        title: 'First',
+        content: '',
+        description: 'bla bla bla',
+        date: new Date(),
+        key:'first',
+        imageUrl: 'https://loremflickr.com/320/240/snail',
+    },
+    {
+        id: 2,
+        title: 'Second',
+        content: '',
+        description: 'bla bla bla bla',
+        date: new Date(),
+        key:'second-article',
+        imageUrl: 'https://loremflickr.com/320/240/dog',
+    },
+    {
+        id: 3,
+        title: 'Thirf',
+        content: '',
+        description: 'bla bla bla bla bla',
+        date: new Date(),
+        key:'third-article',
+        imageUrl: 'https://loremflickr.com/320/240/grass',
+    }
+];
+
+
+
